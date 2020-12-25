f6075b4d481d653b04c6fff4564f417bdf1e02ab
MoonKabir <45933210+moonkabir@users.noreply.github.com>
2020-12-25 23:34:39 +0600

    Add files via upload

---
+++ 29  funtional/Untitled1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int moon(int n)
+{
+ int i;
+for(i=1;i<=n;i++)
+printf("moon kabir");
+}
+int max(x,y)
+{
+printf("enter x and y:");
+scanf("%d %d",&x,&y);
+if(x>y)
+printf("%d max,%d min",x,y);
+else
+printf("%d max,%d min",y,x);
+}
+main()
+{
+max(x,y);
+moon();
+}
---
+++ while loop with scan f/Untitled3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int m;
+printf("Enter the integer number:\n");
+scanf("%d",&m);
+printf("cse\n");
+while(m<=10)
+{
+printf("sbpgc\n");
+printf("%d\n",m);
+m++;
+}
+printf("%d\n",m);
+printf("Radio Today\n");
+}
